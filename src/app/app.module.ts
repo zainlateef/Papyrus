ba1da0e5db4e684e65735a15e0d1d82acddd434a
Zain Lateef <zainlateef@Zains-MacBook-Pro.local>
2018-04-25 21:23:20 -0400

    starting work on dragging nav items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-
+import { DragulaModule } from 'ng2-dragula';
+import { ExampleComponent } from './example/example.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ExampleComponent
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    DragulaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
