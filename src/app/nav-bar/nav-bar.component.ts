6a45e02adddc0185ec4b7992d11566a36295724b
Zain Lateef <zainlateef@Zains-MacBook-Pro.local>
2018-04-25 22:08:10 -0400

    fixed nav bar dragging animation. Still have to fix dot

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
-import * as $ from 'jquery';
+import { DragulaService } from 'ng2-dragula';
 
 @Component({
   selector: 'app-nav-bar',
@@ -11,7 +11,12 @@ export class NavBarComponent implements OnInit {
   @ViewChild("nav__list") menu : ElementRef;
   @ViewChild("burger") burger : ElementRef;
 
-  constructor() { }
+  constructor(private dragulaService: DragulaService) {
+    dragulaService.drop.subscribe((value) => {
+      console.log(`drop: ${value[0]}`);
+      this.onDrop(value.slice(1));
+    });
+  }
 
   ngOnInit() {
     
@@ -21,6 +26,14 @@ export class NavBarComponent implements OnInit {
     console.log("here");
     this.burger.nativeElement.classList.toggle('burger--active');
     this.menu.nativeElement.classList.toggle('nav__list--active');
+  }
+
+  private onDrop(args) {
+    let [e, el] = args;
+    console.log("dropped"+JSON.stringify(args));
+    // TODO: test whether drag and drop persists automatically 
+    // persists, or do I have to somehow parse the dom for the ordering
+    // do something
   }
 
 }
