c4cf6b0f50c08c0bfb92e997fe530ce6278c3af7
Zain Lateef <zainlateef@USF-Gold-Wifi-226-101-93.laptops.usf.edu>
2018-04-26 16:23:18 -0400

    Trying to format upper nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { DragulaModule } from 'ng2-dragula';
-import { ExampleComponent } from './example/example.component'
+import { ExampleComponent } from './example/example.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { UserMenuComponent } from './user-menu/user-menu.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ExampleComponent
+    ExampleComponent,
+    SearchBarComponent,
+    UserMenuComponent
   ],
   imports: [
     BrowserModule,
